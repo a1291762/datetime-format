ac6b60666024cccc7625cdbb70c559d80f26944f
Lincoln Ramsay <a1291762@gmail.com>
2021-09-23 09:13:04 +1000

    Change the status bar on every panel (Dash2Panel)

--- src/formatTargets/StatusBar.js
+++ src/formatTargets/StatusBar.js
@@ -6,26 +6,73 @@ const name = {
 
 const defaultFormat = "%c";
 
-let clockDisplay;
-let dateTimeDisplay;
+let clockMap = new Map();
 
 function init() {
-	clockDisplay = imports.ui.main.panel.statusArea.dateMenu._clockDisplay;
-	dateTimeDisplay = new imports.gi.St.Label({
-		y_align: imports.gi.Clutter.ActorAlign.CENTER
-	});
 }
 
 function enable() {
-	clockDisplay.hide();
-	clockDisplay.get_parent().insert_child_below(dateTimeDisplay, clockDisplay);
+	//global.log("StatusBar enable");
+	eachClock().forEach(clockDisplay => {
+		//global.log(`StatusBar enable clock ${clockDisplay}`);
+		if (clockMap.has(clockDisplay)) {
+			//global.log(`StatusBar Why is clockDisplay in clockMap?! ${JSON.stringify(clockMap)}`);
+		} else {
+			let label = new imports.gi.St.Label({
+				y_align: imports.gi.Clutter.ActorAlign.CENTER,
+				// justify: imports.gi.Clutter.ActorAlign.CENTER,
+			});
+			clockDisplay.hide();
+			clockDisplay.get_parent().insert_child_below(label, clockDisplay);
+			clockMap.set(clockDisplay, label);
+		}
+	});
+
+	if (global.dashToPanel && ! global.dashToPanel._dateTimeformatPanelsCreatedId) {
+		global.dashToPanel._dateTimeformatPanelsCreatedId = global.dashToPanel.connect('panels-created', () => enable());
+	}
 }
 
 function disable() {
-	clockDisplay.show();
-	clockDisplay.get_parent().remove_child(dateTimeDisplay);
+	//global.log("StatusBar disable");
+	eachClock().forEach(clockDisplay => {
+		//global.log(`StatusBar disable clock ${clockDisplay}`);
+		if (clockMap.has(clockDisplay)) {
+			let label = clockMap.get(clockDisplay);
+			clockMap.delete(clockDisplay);
+			clockDisplay.show();
+			clockDisplay.get_parent().remove_child(label);
+		} else {
+			//global.log(`StatusBar Why isn't clockDisplay in clockMap?! ${JSON.stringify(clockMap)}`);
+		}
+	});
+
+	if (global.dashToPanel && global.dashToPanel._dateTimeformatPanelsCreatedId) {
+		global.dashToPanel.disconnect(global.dashToPanel._dateTimeformatPanelsCreatedId);
+		delete global.dashToPanel._dateTimeformatPanelsCreatedId;
+	}
 }
 
 function update(format) {
-	dateTimeDisplay.set_text(format);
+	//global.log("StatusBar update");
+	eachClock().forEach(clockDisplay => {
+		//global.log(`StatusBar update clock ${clockDisplay}`);
+		if (clockMap.has(clockDisplay)) {
+			let label = clockMap.get(clockDisplay);
+			label.set_text(format);
+		} else {
+			//global.log(`StatusBar Why isn't clockDisplay in clockMap?! ${JSON.stringify(clockMap)}`);
+		}
+	});
+}
+
+function eachClock() {
+	let ret = [];
+	let panelArray = global.dashToPanel ? global.dashToPanel.panels.map(pw => pw.panel || pw) : [imports.ui.main.panel];
+	let iterLength = panelArray.length;
+	for(var index = 0; index < iterLength; index++){
+		let panel = panelArray[index];
+		ret.push(panel.statusArea.dateMenu._clockDisplay);
+	}
+	return ret;
 }
